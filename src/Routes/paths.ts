85db0771f8493ac3aeeb9f5dbc2f2565455e0153
krishnaSaiSanga <krishnasaisanga2@gmail.com>
2024-06-12 10:29:48 +0530

    fixed removeitem bug

--- src/Routes/paths.ts
+++ src/Routes/paths.ts
@@ -32,7 +32,7 @@ function setCookies(req: Request, res: Response, next: any) {
   res.cookie("items", items, {
     httpOnly: true,
     secure: true,
-    sameSite: "lax",
+    sameSite: "none",
   });
 
   next();
@@ -70,7 +70,7 @@ function removeItemFromCookie(req: Request, res: Response, next: any) {
     res.cookie("items", items, {
       httpOnly: true,
       secure: true,
-      sameSite: "lax",
+      sameSite: "none",
     });
     // -----------------------------Jwt Verification-----------------
     const result = req.cookies.items.map((itemToken: any) =>
